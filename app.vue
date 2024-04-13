4cbbf662a2ea3b8a0d6a72de0b5f09c3627d75d4
Rukio <xingma20@gmail.com>
2024-04-13 23:26:31 +0300

    Добавил рендер элиминейшен-схемы с мок-данными

---
+++ app.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <NuxtPage />
+  </div>
+</template>
+<style lang="sass">
+@use "~/assets/styles/normalize.css"
+@use "~/assets/styles/reset.css"
+
+html
+  font-size: 10px
+
+body
+  background-color: black
+  color: white
+  font-size: 1.3rem
+</style>
---
+++ pages/tourney/[id].vue
@@ -0,0 +1,77 @@
+<template>
+	<div class="phases">
+		<div v-for="phase in phasesResult" classes="phase">
+			<div v-if="phase.type === 1" class="phase-elimination">
+				<div v-for="tour in phase.tours" class="phase-tour">
+					<div v-for="game in tour" class="phase-game">
+						<div class="game-teams">
+							<div v-for="team in game.teams" class="game-team">
+								<div class="team-players">
+									<div v-for="player in team.players" class="team-player">{{ player.name }}</div>
+								</div>
+								<div class="team-score">{{ team.score || '-' }}</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script setup>
+import { graphElimination } from './mocks';
+
+const graph = graphElimination;
+const phasesResult = [];
+
+const phases = graph.phases;
+
+phases.forEach(phase => {
+	const data = phase;
+	const tourCount = phase.games.reduce((acc, game) => game.tour > acc ? game.tour : acc, 0);
+
+	data.tours = [];
+
+	for (let i = 1; i < tourCount + 1; i++) {
+		data.tours.push(phase.games.filter(game => game.tour === i));
+	}
+
+	phasesResult.push(data);
+});
+
+console.log(phasesResult);
+</script>
+
+<style lang="sass">
+	.phase:not(:last-child)
+		margin-bottom: 25px
+
+	.phase-elimination
+		display: flex
+
+	.phase-tour
+		display: flex
+		flex-direction: column
+		justify-content: center
+		margin-right: 4rem
+
+	.phase-game
+		border: 0.1rem solid white
+		margin-bottom: 4rem
+
+	.game-team
+		padding: 0.5rem
+		display: flex
+		justify-content: space-between
+		border-bottom: 0.1rem solid white
+
+	.team
+		&-players
+			display: flex
+			margin-right: 1.5rem
+
+		&-player
+			&:not(:last-child)
+				margin-right: 2.5rem
+</style>
